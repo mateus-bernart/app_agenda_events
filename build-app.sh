bc58be811cd21efaabbf4b2e96ff625b940d6f16
Mateus Bernart <mateusbernart14@gmail.com>
2025-05-28 17:09:11 -0300

    teste

--- build-app.sh
+++ build-app.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-# Make sure this file has executable permissions, run `chmod +x build-app.sh`
-
-# Exit the script if any command fails
 set -e
+set -x
 
-# Build assets using NPM
+composer install --no-interaction --prefer-dist --optimize-autoloader
+php artisan config:clear
+php artisan migrate --force
+
+npm install
 npm run build
-
-# Clear cache
-php artisan optimize:clear
-
-# Cache the various components of the Laravel application
-php artisan config:cache
-php artisan event:cache
-php artisan route:cache
-php artisan view:cache
