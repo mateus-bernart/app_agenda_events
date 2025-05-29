08505950cb3a5659bcda66c1a5c3d282d4bc77d6
Mateus Bernart <mateusbernart14@gmail.com>
2025-05-29 08:13:29 -0300

    t

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
